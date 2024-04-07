f4a51c8bd0734e7874fc9450ef22b1b4e1e4cb18
Pedro Alberto <pedroalbertompontes@gmail.com>
2024-04-07 20:06:23 -0300

    added the put route

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -10,27 +10,27 @@ router.get("/games", (req, res) => {
 })
 
 router.get("/games/:id", (req, res) => {
-    if(isNaN(req.params.id)){
+    if (isNaN(req.params.id)) {
         res.sendStatus(400)
-    }else{
+    } else {
         let id = parseInt(req.params.id)
         let game = db.games.find(g => g.id == id)
 
-        if(game != undefined){
+        if (game != undefined) {
             res.json(game)
-        }else{
+        } else {
             res.sendStatus(204)
         }
     }
 })
 
 router.post("/games", (req, res) => {
-    
-    let {id, title, price, year} = req.body
 
-    if(isNaN(id) || id < 0 || isNaN(price) || price < 0 || isNaN(year) || year < 1900 || title === "" || typeof(title) !== "string"){
+    let { id, title, price, year } = req.body
+
+    if (isNaN(id) || id < 0 || isNaN(price) || price < 0 || isNaN(year) || year < 1900 || title === "" || typeof (title) !== "string") {
         res.sendStatus(204)
-    }else{
+    } else {
         db.games.push({
             id,
             title,
@@ -43,17 +43,52 @@ router.post("/games", (req, res) => {
 })
 
 router.delete("/games/:id", (req, res) => {
-    if(isNaN(req.params.id)){
+    if (isNaN(req.params.id)) {
         res.sendStatus(400)
-    }else{
+    } else {
         let id = parseInt(req.params.id)
         let index = db.games.findIndex(g => g.id == id)
 
-        if(index == -1){
+        if (index == -1) {
             res.sendStatus(204)
-        }else{
+        } else {
             db.games.splice(index, 1)
             res.sendStatus(200)
+        }
+    }
+})
+
+router.put("/games/:id", (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)
+    } else {
+        let id = parseInt(req.params.id)
+        let game = db.games.find(g => g.id == id)
+
+        if (game != undefined) {
+            let {title, price, year } = req.body
+
+            if(title != undefined){
+                if(title === "" || typeof (title) != "string"){
+                    res.sendStatus(204)
+                }
+                game.title = title
+            }
+            if(year != undefined){
+                if(isNaN(year) || year < 1900){
+                    res.sendStatus(204)
+                }
+                game.year = year
+            }
+            if(price != undefined){
+                if(isNaN(price) || price < 0){
+                    res.sendStatus(204)
+                }
+                game.price = price
+            }
+            res.sendStatus(200)
+        } else {
+            res.sendStatus(204)
         }
     }
 })
